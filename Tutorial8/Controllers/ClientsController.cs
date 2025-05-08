bf8c07f584630e68aefc9c626c55b07c6520ad2d
jszbm <jorgenszachowicz@gmail.com>
2025-05-09 00:32:09 +0200

    Start implementing clients functionality

--- Tutorial8/Controllers/ClientsController.cs
+++ Tutorial8/Controllers/ClientsController.cs
@@ -6,8 +6,20 @@ namespace Tutorial8.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ClientsController
+    public class ClientsController : ControllerBase
     {
-    
+        private readonly IClientsService _clientsService;
+
+        public ClientsController(IClientsService clientsService)
+        {
+            _clientsService = clientsService;
+        }
+        
+        [HttpGet("{id}/trips")]
+        public async Task<IActionResult> GetClientTrips(int id)
+        {
+            var clientTrips = await _clientsService.GetClientTrips(id);
+            return Ok(clientTrips);
+        }
     }
 }
